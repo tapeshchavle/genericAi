c313a4194ea25f9174f5a5e98e0b347fcaa5859f
tapeshchavle <tapeshchawle@gmail.com>
2026-05-12 17:52:51 +0530

    fixed cors origin

--- src/main/java/com/genericAI/ChatController.java
+++ src/main/java/com/genericAI/ChatController.java
@@ -3,6 +3,7 @@ package com.genericAI;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ChatController {
 
     private final ChatModel chatModel;
